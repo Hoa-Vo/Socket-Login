832725200e8ed15946c0cf898a9713e051154de7
AN VÕ THIỆN <voan0935995700@gmail.com>
2020-06-09 15:37:07 +0700

    add sending function

--- server.py
+++ server.py
@@ -1,11 +1,45 @@
 import socket
 import sys
-import cgi
 
-# serverSocket.close()
-# sys.exit()
-# Kích hoạt server
 
+
+
+def send_html_file(socket, filename):
+    try:
+        http_header1 = "HTTP/1.1 200 OK \r\n"
+        http_header2 = http_header2 = "Content-Type: text/html \r\n"
+        file_stream = open(filename)
+        
+        sending_data = file_stream.read()
+
+        print("Find found.")
+        socket.send(http_header1.encode())
+        socket.send(http_header2.encode())
+        socket.send("\r\n".encode())
+
+        for i in range(0, len(sending_data)):
+            socket.send(sending_data[i].encode())
+        
+        socket.send("\r\n".encode())
+        print("File sent: " + filename)
+    except IOError: # khi không tìm thấy file
+        print("Can't find the file, send 404")
+        file_stream = open("404.html")
+        sending_data = file_stream.read()
+
+
+        http_error_header = "HTTP/1.1 404 Not Found\r\n"
+        socket.send(http_error_header.encode())
+        socket.send("\r\n".encode())
+
+        for i in range(0, len(sending_data)):
+            socket.send(sending_data[i].encode())
+        
+        socket.send("\r\n".encode())
+        print("File sent: " + "404.html")
+
+        
+        
 
 def activeServer(serverAddres, serverPort):
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,23 +49,21 @@ def activeServer(serverAddres, serverPort):
     while True:
         connectionSocket, connectionAddress = serverSocket.accept()
         print("Received a request from Address: {0}".format(connectionAddress))
-        try:
-            massage = connectionSocket.recv(2048).decode()
-            massages = massage.split(' ')
-            method = massages[0]
-            if(method == "GET"):
-                sendLoginForm(connectionSocket)
-            elif(method == "POST"):
-                string = massages.pop()
-                splitArr = string.split("\n")
-                string = splitArr[len(splitArr)-1]
-                temp = getUsernameAndPassword(string)
-                checkUsernameAndPassword(temp[0], temp[1])
-                print(temp[0]+" "+temp[1])
-                sendInfoFile(connectionSocket)
+        massage = connectionSocket.recv(2048).decode()
+        massages = massage.split(' ')
+        method = massages[0]
+        if(method == "GET"):
+            send_html_file(connectionSocket,"login.html")
+        elif(method == "POST"):
+            string = massages.pop()
+            splitArr = string.split("\n")
+            string = splitArr[len(splitArr)-1]
+            temp = getUsernameAndPassword(string)
+            checkUsernameAndPassword(temp[0], temp[1])
+            print(temp[0]+" "+temp[1])
+            send_html_file(connectionSocket, "info.html")
             connectionSocket.close()
-        except IOError:
-            pass
+        
 
 
 

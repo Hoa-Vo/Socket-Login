1dd94cf6404cd1ef773b645ecb935e9d035b0c4b
VoHoa-HCMUS <vohoa1468@gmail.com>
2020-06-10 13:49:22 +0700

    fix bug

--- server.py
+++ server.py
@@ -5,13 +5,14 @@ import sys
 def send_html_file(socket, filename):
     try:
         http_header1 = "HTTP/1.1 200 OK\r\n"
-        http_header2 = http_header2 = "Content-Type: text/html\r\n"
+        http_header2 = "Content-Type: text/html\r\n"
         file_stream = open(filename)
 
         sending_data = file_stream.read()
 
         print("Find found.")
         socket.send(http_header1.encode())
+        socket.send(http_header2.encode())
         socket.send("\r\n".encode())
 
         for i in range(0, len(sending_data)):
@@ -48,9 +49,15 @@ def activeServer(serverAddres, serverPort):
         massages = massage.split(' ')
         method = massages[0]
         if(method == "GET"):
-            redirect(connectionSocket, "/login.html")
+            if(count == 0):
+                redirect(connectionSocket, "/login.html")
+                count = 1
             if(massages[1] == "/login.html"):
                 send_html_file(connectionSocket, "login.html")
+            if(massages[1] == "/info.html"):
+                send_html_file(connectionSocket, "info.html")
+            if(massages[1] == "/404.html"):
+                send_html_file(connectionSocket, "404.html")
         elif(method == "POST"):
             string = massages.pop()
             splitArr = string.split("\n")
@@ -60,9 +67,9 @@ def activeServer(serverAddres, serverPort):
             true_login_info = checkUsernameAndPassword(temp[0], temp[1])
 
             if(true_login_info):
-                send_html_file(connectionSocket, "info.html")
+                redirect(connectionSocket, "/info.html")
             else:
-                send_html_file(connectionSocket, "404.html")
+                redirect(connectionSocket, "/404.html")
             connectionSocket.close()
 
 
